8b029e93494115a6610bdcef18ebbd3fa6f33df0
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-30 11:06:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿List<int> numeros = new List<int> { 1, 2, 3, 2, 4, 5, 3, 6, 7, 8, 9, 1, 5, 67, 3, 4 };
+
+var numerosUnicos = numeros.Distinct();
+
+Console.WriteLine("Números únicos da lista: ");
+foreach (var  numero in numerosUnicos) {
+    Console.WriteLine($" -  {numero}");
+}
